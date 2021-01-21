8bd8d4c55239674f8864ff1bb3d295d34df3654c
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-01-22 07:11:36 +0900

    updateの方法変えた

--- src/cron.ts
+++ src/cron.ts
@@ -1,6 +1,6 @@
+import dayjs from 'dayjs';
 import { Client, Guild, TextChannel } from 'discord.js';
 import { config } from 'dotenv';
-import { getConnection } from 'typeorm';
 import { getFortune, sendResult } from './bots/OmikujiBot';
 import { BoResult } from './entity/BoResult';
 import { Omikuji } from './entity/Omikuji';
@@ -11,6 +11,7 @@ const OMIKUJI_TOKEN = process.env.TEST_TOKEN as string;
 const MARUOKUN_ID = process.env.MARUOKUN_ID as string;
 const CHANNEL_ID = process.env.MAIN_CHANNEL_ID as string;
 const userId = '718705193373532160';
+const id = 2;
 
 const client = new Client();
 client.on('ready', () => onReady());
@@ -19,26 +20,18 @@ const onReady = async () => {
   const fortune = getFortune();
   await connect(Omikuji, (repository) => repository.increment({ userId }, fortune, 1));
   const { result } = (await connect(BoResult, (repository) =>
-    repository.findOne({ id: 1 })
+    repository.findOne({ id }),
   )) as BoResult;
   if (result === fortune) {
-    await updateTime();
+    const now = dayjs().format('YYYY-MM-DD HH:mm:ss.SSS');
+    await connect(BoResult, (repository) => repository.save({ id, updatedAt: now }));
   } else {
-    await connect(BoResult, async (repository) => repository.save({ id: 1, result: fortune }));
+    await connect(BoResult, (repository) => repository.save({ id, result: fortune }));
   }
   const guild = client.guilds.cache.find(({ id }) => id === MARUOKUN_ID) as Guild;
   const channel = guild.channels.cache.find(({ id }) => id === CHANNEL_ID) as TextChannel;
   await sendResult(channel, fortune);
   client.destroy();
 };
-
-async function updateTime() {
-  await getConnection()
-    .createQueryBuilder()
-    .update(BoResult)
-    .set({ updatedAt: 'NOW()' })
-    .where('id = 1')
-    .execute();
-}
 
 client.login(OMIKUJI_TOKEN);
