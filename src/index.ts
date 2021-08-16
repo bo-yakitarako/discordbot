4a8a85754c2aaa597a6479ffb84bac3d68b6c442
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-08-16 13:54:01 +0900

    SGC停止

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import 'reflect-metadata';
 import { OmikujiBot } from './bots/OmikujiBot';
-import { SparebeatGradeCertification } from './bots/SparebeatGradeCertification';
+// import { SparebeatGradeCertification } from './bots/SparebeatGradeCertification';
 import { GoodBot } from './bots/GoodBot';
 import { ScoreAttackBot } from './bots/ScoreAttackBot';
 
@@ -10,9 +10,9 @@ const OMIKUJI_TOKEN = process.env.OMIKUJI_TOKEN as string;
 const omikuji = new OmikujiBot();
 omikuji.launch(OMIKUJI_TOKEN);
 
-const SGC_TOKEN = process.env.SGC_TOKEN as string;
-const sgc = new SparebeatGradeCertification();
-sgc.launch(SGC_TOKEN);
+// const SGC_TOKEN = process.env.SGC_TOKEN as string;
+// const sgc = new SparebeatGradeCertification();
+// sgc.launch(SGC_TOKEN);
 
 const GOOD_TOKEN = process.env.GOOD_TOKEN as string;
 const good = new GoodBot();
